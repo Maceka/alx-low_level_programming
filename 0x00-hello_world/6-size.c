a1994abe3256d779002bce930f2657bf0ce93aac
Donald Umeh <donaldumeh54@gmail.com>
2022-03-10 12:46:54 -0800

    corrected the error on 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  **/
+
+
 int main(void)
 {
-char c;
-int n;
-long int t;
-long long int x;
-float q;
-printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of a int: %lu byte(s)\n", sizeof(n));
-printf("Size of a long int: %lu byte(s)\n", sizeof(t));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(x));
-printf("Size of a float: %lu byte(s)\n", sizeof(q));
+char a_character;
+int a_integer;
+long a_long;
+long long a_longlong ;
+float a_float;
+printf("Size of a char: %lu byte(s)\n", sizeof(a_character));
+printf("Size of a int: %lu byte(s)\n", sizeof(a_integer));
+printf("Size of a long int: %lu byte(s)\n", sizeof(a_long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(a_longlong));
+printf("Size of a float: %lu byte(s)\n", sizeof(a_float));
 }
