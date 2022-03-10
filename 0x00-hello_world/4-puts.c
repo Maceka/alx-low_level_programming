d71d04f4c845e568ff85e1cfe9365264aa99417c
Donald Umeh <donaldumeh54@gmail.com>
2022-03-10 09:42:21 -0800

    created a C file and compiled it only

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Returns: "Programming is like building a multilingual puzzle
+ **/
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
