84a99acee8c1e5326979c89033b524efe993917a
Donald Umeh <donaldumeh54@gmail.com>
2022-03-10 12:39:11 -0800

    corrected my error in 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,5 +16,5 @@ printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of a int: %lu byte(s)\n", sizeof(n));
 printf("Size of a long int: %lu byte(s)\n", sizeof(t));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(x));
-printf("Size of a float: %lu byte(s)", sizeof(q));
+printf("Size of a float: %lu byte(s)\n", sizeof(q));
 }
