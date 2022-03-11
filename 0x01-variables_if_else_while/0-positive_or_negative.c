2510ffeff52a567624d4e7398c2b6fab5bddf16f
Donald Umeh <donaldumeh54@gmail.com>
2022-03-11 08:38:14 -0800

    corrected my error in 0-positive task on if and else if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-
-/*
-	 * main function - Entry point
-	 * 
-	 * Please use it consistently.
-	 *
-	 * Description:  This function checks the n random number and
-	 * outputs if it's negative, positive or zero
-	 **/
+/**
+ * main - assigns a random number to int n
+ * it executes the program, and prints n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else
-{
-printf("%d is zero\n", n);
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
