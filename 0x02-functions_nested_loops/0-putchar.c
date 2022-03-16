fbf38ab57cc9ba486bb34aba738d0fb8b22d8373
Donald Umeh <donaldumeh54@gmail.com>
2022-03-15 23:41:53 -0700

    printed a set of strings using header file as prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * main - print out the string
- * _putchar
+ * main - prints string "_putchar" using
+ * my own header file and putchar() function prototype defined in it
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char str[] = "_putchar";
-
 	int i = 0;
 
 	while (str[i])
 	{
 		_putchar(str[i]);
-		++i;
+		i++;
 	}
 	_putchar('\n');
 	return (0);
