5848a5ad4edfd2c6b96bfcbb04b20cb7a7db3ea1
Donald Umeh <donaldumeh54@gmail.com>
2022-03-15 22:29:01 -0700

    prints the string with my header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print out the string
+ * _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		++i;
+	}
+	_putchar('\n');
+	return (0);
+}
