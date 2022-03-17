a261c76dfc2823e2697dfac7e9bfdaaa28e95799
Donald Umeh <donaldumeh54@gmail.com>
2022-03-17 13:53:15 -0700

    prints a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print lines the number of time in arg
+ * @n: number of time to be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
