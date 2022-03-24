37b9ebd45c3680e47bebded0134a42e16da35546
Donald Umeh <donaldumeh54@gmail.com>
2022-03-24 02:14:21 -0700

    string concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatinates the strings together
+ * @dest: the destination of the string
+ * @src: The source where the string will be added from
+ * Return: a pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# Pointers Arrays Strings PART 2
+
+This is the senond task project i was given at The Holberton School Of Software Engineering. In this repo you'll find the answers to the task given and more.
+
+Written By:
+Donald
